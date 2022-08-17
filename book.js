1968fe44ae9e4b9eae726ef897aa63e722ea22c2
Eugene Zhang <zhang.eug@gmail.com>
2022-08-16 23:11:52 -0400

    Add new book functionality

--- book.js
+++ book.js
@@ -4,15 +4,55 @@ function createBook(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = () => {
-        return `${title} by ${author}, ${pages} pages, ${read}`
+        return `${title} by ${author}, ${pages} pages`
     };
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(user) {
+  LIBRARY.push(user);
 }
 
-// const book = new createBook('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
-// console.log(book.info());
+function displayBook() {
+  const body = document.querySelector(".body");
+  body.innerHTML = '';
 
-let myLibrary = [];
+  for (let book of LIBRARY) {
+    const bookContainer = document.createElement('div');
+    bookContainer.classList.add("card");
+    bookContainer.innerText = book.info();
+    body.appendChild(bookContainer);
+  }
+}
+
+function openForm(e) {
+  document.querySelector(".form-container").style.display = "block";
+}
+
+function closeForm(e) {
+  document.querySelector(".form-container").style.display = "none";
+}
+
+function getData(e) {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.querySelector('input[name="read"]:checked').value;
+
+  const aBook = new createBook(title, author, pages, read);
+  
+  document.querySelector(".actual-form").reset();
+
+  addBookToLibrary(aBook);
+
+  displayBook();
+}
+
+const LIBRARY = [];
+
+const addBookBtn = document.querySelector('.add-book');
+addBookBtn.addEventListener('click', openForm);
+
+const buttonCancel = document.querySelector('.button-cancel');
+const buttonAdd = document.querySelector('.button-add');
+buttonCancel.addEventListener('click', closeForm);
+buttonAdd.addEventListener('click', getData);
